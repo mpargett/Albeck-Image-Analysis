6771fcd1f29cfa0684dd410f2890a80c0fd1267a
Michael Pargett <mpargett@ucdavis.edu>
2017-09-18 15:52:12 -0700

    Pathdefs update, works this time

--- iman_pathdefs.m
+++ iman_pathdefs.m
@@ -9,25 +9,27 @@
 function p = iman_pathdefs()
 %Declare proper path for image processing code
 % p.code = '';  %Default sets no new path
-p.code = ['\\albecklab.mcb.ucdavis.edu\Code\Image Analysis\'];
+p.code = ['\\albecklab.mcb.ucdavis.edu\data\Code\Image Analysis\'];
 %     ['\\albecklab.mcb.ucdavis.edu\data\Code\Image Analysis\Test Scripts\'];
         
 %Declare proper path for Bio-Formats package
 % p.bioformats = '';    %Default sets no new path, but must be set unless
 %                       bioformats.jar is in the Working Directory when
 %                       processing is run
-p.bioformats = '\\albecklab.mcb.ucdavis.edu\bfmatlab\';
+p.bioformats = '\\albecklab.mcb.ucdavis.edu\data\bfmatlab\';
 
 %Declare proper path for uTrack
 % p.utrack = '';    %Default sets no new path
-p.utrack = '\\albecklab.mcb.ucdavis.edu\u-track_2.1.0\software\';
+p.utrack = '\\albecklab.mcb.ucdavis.edu\data\u-track_2.1.0\software\';
         
 %Declare common problematic paths
 %   If desired can list undesirable paths (warning will show when they are
 %   not present), or can search the current path for them via regular
 %   expressions
 pth = path;
-p.bad = regexpi(pth, '[^;]*\\Code[^;]*;', 'match');
-p.bad = [p.bad,regexpi(pth, '[^;]*\\u-track[^;]*;', 'match')];
+p.bad = regexpi(pth, ...
+    '[^;]*\\albecklab.mcb.ucdavis.edu\\data\\Code[^;]*;', 'match');
+p.bad = [p.bad,regexpi(pth, ...
+    '[^;]*\\albecklab.mcb.ucdavis.edu\\data\\u-track[^;]*;', 'match')];
 end
 
